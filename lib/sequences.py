9216fb5f63e9b01db3da1db54691d22a47590dd8
AzharTakoy <azharahmedtakoy4444@gmail.com>
2023-05-16 13:06:41 +0300

    Complete assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,23 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    n1 = 0
+    n2 = 1
+    i = 0
+    fibonacci_list = []  # Create an empty list to store Fibonacci numbers
+
+    if length <= 0:
+        fibonacci_list = []
+    elif length == 1:
+        fibonacci_list = [0]
+    else:
+        while i < length:
+            fibonacci_list.append(n1)  # Append current Fibonacci number to the list
+            nth_term = n1 + n2
+            n1 = n2
+            n2 = nth_term
+            i += 1
+
+    print(fibonacci_list)  # Print the concatenated list of Fibonacci numbers
+
+
+# Test case
+print_fibonacci(2)
